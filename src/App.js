234220a1c1279690642500c55351a4f42285a7cf
moo9205 <angus920514@gmail.com>
2021-06-08 22:14:21 +0900

    practice for react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="blank-nav">
+        <div>Pizzahand Blog</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
